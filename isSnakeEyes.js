cd7a0187905298aaf7dcf503a42db5a97d14d7b1
olivierukundo <rukundolivier7@gmail.com>
2022-09-15 09:20:04 +0200

    Update isSnakeEyes.js
    my code after yours generated an error. (mine didn't work too)

--- isSnakeEyes.js
+++ isSnakeEyes.js
@@ -1,23 +1,40 @@
-// function to check if a roll is a snake eyes
-function isSnakeEyes({ num1, num2 }) {
-  console.log({ num1, num2 });
-  //check snake eyes using ternary operator
-  num1 === 1 && num2 === 1
-    ? console.log("snake eyes!")
-    : console.log("Not Snake Eyes!");
+// // function to check if a roll is a snake eyes
+// function isSnakeEyes({ num1, num2 }) {
+//   console.log({ num1, num2 });
+//   //check snake eyes using ternary operator
+//   num1 === 1 && num2 === 1
+//     ? console.log("snake eyes!")
+//     : console.log("Not Snake Eyes!");
+// }
+
+// // function to generate a random couple
+// function randomCouple() {
+//   num1 = Math.ceil(Math.random() * 6);
+//   num2 = Math.ceil(Math.random() * 6);
+
+//   return { num1, num2 };
+// }
+
+// // const TEST to check the isSnakeEyes function
+
+// const TEST = { num1: 1, num2: 1 };
+
+// isSnakeEyes(TEST);
+// isSnakeEyes(randomCouple());
+
+
+// this is the code i wrote after running yours and got an error
+function isSnakeEyes(firstDig, SecondDig){
+  if(firstDig === 1 && SecondDig === 1){
+      console.log("Snake Eyes")
+  } else if( firstDig === 1 || SecondDig !== 1){
+      console.log("Not Snake Eyes")
+  } else {
+     console.log("Not Snake Eyes")
+  }
 }
 
-// function to generate a random couple
-function randomCouple() {
-  num1 = Math.ceil(Math.random() * 6);
-  num2 = Math.ceil(Math.random() * 6);
+let firstDig = Math.floor(Math.random() * 6) + 1;
+let SecondDig = Math.floor(Math.random() * 6) + 1;
 
-  return { num1, num2 };
-}
-
-// const TEST to check the isSnakeEyes function
-
-const TEST = { num1: 1, num2: 1 };
-
-isSnakeEyes(TEST);
-isSnakeEyes(randomCouple());
+// //  didn't work either i can not tell where i wwent wrong
